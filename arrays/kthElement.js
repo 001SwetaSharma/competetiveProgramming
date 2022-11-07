4a13d1ffebfc228b11affac46f8462f6168bc348
sweta.kumari <sweta.kumari@colortokens.com>
2022-11-07 10:26:49 +0530

    max and min kth element from an array

---
+++ arrays/kthElement.js
@@ -0,0 +1,23 @@
+//K’th smallest element in an unsorted array using sorting:
+
+
+let inputData = [];
+process.stdin.on('data',function(chunk){
+    inputArr += chunk;
+});
+
+
+process.stdin.on('end',function(){
+    inputArr = inputArr.split('\n');
+    let kthEle = parseInt(inputArr[0]);
+    
+    
+    //1st approach
+    // sort and find the kth max and min ele
+    // Time complexity will be O(nlogn) as sort uses tim sort algorithm to sort the array.
+    inputArr = inputArr.sort(function(a,b){ return a-b; });
+    let minEle = inputArr[kthEle - 1];
+    let maxEle = inputArr[inputArr.length - kthEle];
+    console.log(`min Kth element.............. ${minEle}`);
+    console.log(`max Kth element.............. ${maxEle}`);
+});

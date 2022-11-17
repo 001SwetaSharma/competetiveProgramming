a6534c552f04b0eaeb882d1f92645c82f3863785
sweta.kumari <sweta.kumari@colortokens.com>
2022-11-17 22:13:30 +0530

    added second approach for sorting array consisting only of 0 1 and 2

--- arrays/dutchNationalFlagProblem.js
+++ arrays/dutchNationalFlagProblem.js
@@ -1,5 +1,11 @@
+//sort the array without using sort function
+
 let inputArr = [0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1];
 
+
+//1st approach
+//time complexity is O(n)
+//space complexity is O(n)
 let hashTable = {};
 for(let i=0;i<inputArr.length;i++){
     if(hashTable[inputArr[i]]){
@@ -21,3 +27,29 @@ for(let i=0;i<hashTable[1];i++){
 for(let i=0;i<hashTable[2];i++){
     sortedArray.push(2);
 }
+
+
+//2nd approach
+let lo = 0, mid = 0, high = inputArr.length-1, temp = 0;
+while(mid<=high){
+    if(inputArr[mid] == 0){
+        temp = inputArr[mid];
+        inputArr[mid] = inputArr[lo];
+        inputArr[lo] = temp;
+        lo++;
+        mid++;
+    }
+
+    else if(inputArr[mid] == 1){
+        mid++;
+    }else{
+        //if the element is 2
+        temp = inputArr[mid];
+        inputArr[mid] = inputArr[high];
+        inputArr[high] = temp;
+        mid++;
+        high--;
+    }
+}
+
+console.log(`Printing Sorted Array ..... :: ${JSON.stringify(inputArr)}`);
